f9546ed619d77da46b1268691079cc5e57e826ab
Matt Vertescher <mvertescher@gmail.com>
2018-12-30 14:27:08 -0500

    Remove default feature k/v to get CI working

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,34 @@
 #![allow(non_camel_case_types)]
 
 #[cfg(not(any(
+    feature = "stm32f722",
+    feature = "stm32f723",
+    feature = "stm32f732",
+    feature = "stm32f733",
+    feature = "stm32f745",
     feature = "stm32f746",
+    feature = "stm32f756",
+    feature = "stm32f765",
+    feature = "stm32f767",
+    feature = "stm32f769",
+    feature = "stm32f777",
+    feature = "stm32f778",
+    feature = "stm32f779",
     )))]
 compile_error!("This crate requires one of the following device features enabled:
+        stm32f722
+        stm32f723
+        stm32f732
+        stm32f733
+        stm32f745
         stm32f746
+        stm32f756
+        stm32f765
+        stm32f767
+        stm32f769
+        stm32f777
+        stm32f778
+        stm32f779
                 ");
 
 use embedded_hal as hal;
