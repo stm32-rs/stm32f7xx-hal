1c1d48c3f7f240bba8237bcbd16279907eff0472
Matt Vertescher <mvertescher@gmail.com>
2019-06-04 09:51:09 -0400

    Include the correct peripheral access crate for feature flag

--- src/lib.rs
+++ src/lib.rs
@@ -36,8 +36,44 @@ compile_error!("This crate requires one of the following device features enabled
 
 pub(crate) use embedded_hal as hal;
 
+#[cfg(feature = "stm32f722")]
+pub use stm32f7::stm32f7x2 as device;
+
+#[cfg(feature = "stm32f723")]
+pub use stm32f7::stm32f7x3 as device;
+
+#[cfg(feature = "stm32f732")]
+pub use stm32f7::stm32f7x2 as device;
+
+#[cfg(feature = "stm32f733")]
+pub use stm32f7::stm32f7x3 as device;
+
+#[cfg(feature = "stm32f745")]
+pub use stm32f7::stm32f7x5 as device;
+
 #[cfg(feature = "stm32f746")]
 pub use stm32f7::stm32f7x6 as device;
+
+#[cfg(feature = "stm32f756")]
+pub use stm32f7::stm32f7x6 as device;
+
+#[cfg(feature = "stm32f765")]
+pub use stm32f7::stm32f7x5 as device;
+
+#[cfg(feature = "stm32f767")]
+pub use stm32f7::stm32f7x7 as device;
+
+#[cfg(feature = "stm32f769")]
+pub use stm32f7::stm32f7x9 as device;
+
+#[cfg(feature = "stm32f777")]
+pub use stm32f7::stm32f7x7 as device;
+
+#[cfg(feature = "stm32f778")]
+pub use stm32f7::stm32f7x9 as device;
+
+#[cfg(feature = "stm32f779")]
+pub use stm32f7::stm32f7x9 as device;
 
 // Enable use of interrupt macro
 #[cfg(feature = "rt")]
