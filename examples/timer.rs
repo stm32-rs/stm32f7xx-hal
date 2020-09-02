7b8fe4ac94804e547377095e49f1fc654ef9216b
Vadim Kaushan <admin@disasm.info>
2020-09-02 23:48:03 +0300

    Fix NVIC usage

--- examples/timer.rs
+++ examples/timer.rs
@@ -1,6 +1,5 @@
 //! Test the general purpose timers
 
-#![deny(unsafe_code)]
 //#![deny(warnings)]
 #![no_std]
 #![no_main]
@@ -11,6 +10,8 @@ use core::fmt::Write;
 
 use cortex_m_rt::entry;
 use cortex_m_semihosting::hio;
+
+use cortex_m::peripheral::NVIC;
 
 use stm32f7xx_hal::{
     interrupt, pac,
@@ -23,14 +24,14 @@ fn main() -> ! {
     let mut hstdout = hio::hstdout().unwrap();
     writeln!(hstdout, "Starting timer...").unwrap();
 
-    let cp = cortex_m::Peripherals::take().unwrap();
     let dp = pac::Peripherals::take().unwrap();
 
     let mut rcc = dp.RCC.constrain();
     let clocks = rcc.cfgr.freeze();
 
-    let mut nvic = cp.NVIC;
-    nvic.enable(pac::Interrupt::TIM2);
+    unsafe {
+        NVIC::unmask(pac::Interrupt::TIM2);
+    }
     let mut timer = Timer::tim2(dp.TIM2, 1.hz(), clocks, &mut rcc.apb1);
     timer.listen(Event::TimeOut);
 
