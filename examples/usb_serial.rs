caf2196fcfb10a2947b255f0437a4e9afe99000e
Vadim Kaushan <admin@disasm.info>
2020-09-17 21:29:55 +0300

    rustfmt

--- examples/usb_serial.rs
+++ examples/usb_serial.rs
@@ -39,7 +39,10 @@ fn main() -> ! {
         dp.OTG_FS_GLOBAL,
         dp.OTG_FS_DEVICE,
         dp.OTG_FS_PWRCLK,
-        (gpioa.pa11.into_alternate_af10(), gpioa.pa12.into_alternate_af10()),
+        (
+            gpioa.pa11.into_alternate_af10(),
+            gpioa.pa12.into_alternate_af10(),
+        ),
         clocks,
     );
     #[cfg(feature = "usb_hs")]
@@ -47,7 +50,10 @@ fn main() -> ! {
         dp.OTG_HS_GLOBAL,
         dp.OTG_HS_DEVICE,
         dp.OTG_HS_PWRCLK,
-        (gpiob.pb14.into_alternate_af12(), gpiob.pb15.into_alternate_af12()),
+        (
+            gpiob.pb14.into_alternate_af12(),
+            gpiob.pb15.into_alternate_af12(),
+        ),
         clocks,
     );
 
