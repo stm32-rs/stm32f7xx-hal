0975ef6fe39754fba5c3e4af234a3100b84a429e
7h3qu1rkyb1t <the.qu1rkyb1t@gmail.com>
2020-11-24 09:30:40 +0530

    random number generation implimented

---
+++ examples/rng.rs
@@ -0,0 +1,22 @@
+#![deny(unsafe_code)]
+#![deny(warnings)]
+#![no_main]
+#![no_std]
+
+/// generates a random number and displays it to the openocd console
+use panic_semihosting as _;
+
+use cortex_m_rt::entry;
+use cortex_m_semihosting::hprintln;
+use stm32f7xx_hal::{pac, prelude::*};
+
+#[entry]
+fn main() -> ! {
+    let p = pac::Peripherals::take().unwrap();
+    let mut rng = p.RNG.init();
+    let val = rng.get_rand().unwrap();
+    hprintln!("random value {}", val).unwrap();
+    loop {
+        core::sync::atomic::spin_loop_hint();
+    }
+}
