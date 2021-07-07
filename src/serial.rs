a8c9e3bdf3dadf8cbd2ba30702663a43642d87ad
Hanno Braun <hanno@braun-odw.eu>
2021-07-07 12:42:43 +0000

    Replace `into_iter` with `iter`
    This is a bit more explicit, as `into_iter` and `iter` have the same
    behavior on slices (nothing is consumed). Fixes a Clippy warning.

--- src/serial.rs
+++ src/serial.rs
@@ -459,11 +459,7 @@ where
     Tx<USART>: serial::Write<u8>,
 {
     fn write_str(&mut self, s: &str) -> fmt::Result {
-        let _ = s
-            .as_bytes()
-            .into_iter()
-            .map(|c| block!(self.write(*c)))
-            .last();
+        let _ = s.as_bytes().iter().map(|c| block!(self.write(*c))).last();
         Ok(())
     }
 }
