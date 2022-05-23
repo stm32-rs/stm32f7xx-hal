69ecdf31772bee137fff0061d28952b12f726aff
Michael Williams <michael.williams@enspiral.com>
2022-05-23 15:10:18 +1200

    add serial parity example

---
+++ examples/serial_parity.rs
@@ -0,0 +1,55 @@
+//! Write characters to the serial port with parity.
+//!
+//! Note: This example is for the STM32F767
+
+#![deny(unsafe_code)]
+#![deny(warnings)]
+#![no_main]
+#![no_std]
+
+extern crate panic_halt;
+
+use nb::block;
+
+use cortex_m_rt::entry;
+use stm32f7xx_hal::{
+    pac,
+    prelude::*,
+    serial::{self, Serial},
+};
+
+#[entry]
+fn main() -> ! {
+    let p = pac::Peripherals::take().unwrap();
+
+    let rcc = p.RCC.constrain();
+    let clocks = rcc.cfgr.sysclk(216_000_000.Hz()).freeze();
+
+    let gpiod = p.GPIOD.split();
+
+    let tx = gpiod.pd5.into_alternate();
+    let rx = gpiod.pd6.into_alternate();
+
+    let serial = Serial::new(
+        p.USART2,
+        (tx, rx),
+        &clocks,
+        serial::Config {
+            baud_rate: 56_700.bps(),
+            oversampling: serial::Oversampling::By16,
+            character_match: None,
+            sysclock: false,
+            parity: serial::Parity::ParityEven,
+        },
+    );
+
+    let (mut tx, mut _rx) = serial.split();
+
+    let mut byte: u8 = 0;
+
+    loop {
+        block!(tx.write(byte)).ok();
+
+        byte += 1;
+    }
+}
